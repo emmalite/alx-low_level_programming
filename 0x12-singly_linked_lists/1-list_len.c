4c91fb39d5f1e5d55ce5037c8102164bbcd54c75
emmalite <emmanuelemafolomi@gmail.com>
2022-05-25 22:09:05 -0700

    completed task Alx project singly linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * @brief This code calculate the numbers of element in a list
+ * 
+ * list_len - Calculate the number of elements.
+ * @h: Pointer to a list.
+ * Return: Integer.
+ **/
+
+size_t list_len(const list_t *h)
+{
+	const list_t *temp;
+	unsigned int counter = 0;
+
+	temp = h;
+	while (temp)
+	{
+		counter++;
+		temp = temp->next;
+	}
+	return (counter);
+}
